4adec9671d2c0094fbb5a56b90868c217d474867
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-12-18 23:00:59 -0700

    dynamic styling

---
+++ vue101dynamicstyle/app.js
@@ -0,0 +1,35 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      boxAselected: false,
+      boxBselected: false,
+      boxCselected: false,
+      boxDselected: false,
+      boxEselected: false,
+    };
+  },
+  computed: {
+    boxCclasses() {
+      return {
+        active: this.boxCselected,
+      };
+    },
+  },
+  methods: {
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxAselected = !this.boxAselected;
+      } else if (box === "B") {
+        this.boxBselected = !this.boxBselected;
+      } else if (box === "C") {
+        this.boxCselected = !this.boxCselected;
+      } else if (box === "D") {
+        this.boxDselected = !this.boxDselected;
+      } else if (box === "E") {
+        this.boxEselected = !this.boxEselected;
+      }
+    },
+  },
+});
+
+app.mount("#styling");
